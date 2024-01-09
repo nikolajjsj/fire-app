fe972a20bf3bcf604c317a149eb5430f7f293add
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2024-01-09 18:15:45 +0100

    feat(packages): add needed packages

---
+++ fire/lifecycle/Defaults.swift
@@ -0,0 +1,15 @@
+//
+//  Defaults.swift
+//  fire
+//
+//  Created by Nikolaj Johannes Skole Jensen on 09/01/2024.
+//
+
+import Foundation
+import Defaults
+
+// TODO: Add defaults
+extension Defaults.Keys {
+    static let quality = Key<Double>("quality", default: 0.8)
+    //           Key          Type   UserDefaults name   Default value
+}
