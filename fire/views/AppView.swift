710920f9997eefc1377ec96d1aa4dd7b2fb7c729
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2024-01-09 18:14:36 +0100

    chore: remove bloat

---
+++ fire/views/AppView.swift
@@ -0,0 +1,18 @@
+//
+//  AppView.swift
+//  fire
+//
+//  Created by Nikolaj Johannes Skole Jensen on 09/01/2024.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    
+    var body: some View {
+    }
+}
+
+#Preview {
+    AppView()
+}
