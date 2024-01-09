2df5a609cd2f7381eb8181545c08f5ccf57f2c0d
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2024-01-09 13:57:55 +0100

    Initial Commit

---
+++ fire/fireApp.swift
@@ -0,0 +1,17 @@
+//
+//  fireApp.swift
+//  fire
+//
+//  Created by Nikolaj Johannes Skole Jensen on 09/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct fireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
